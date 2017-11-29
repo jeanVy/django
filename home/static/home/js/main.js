5a9aa45e0299f6fb1c47836a1d3466c2a9ae22c1
ivy jean dilig <diligivyjean@gmail.com>
2017-11-29 21:40:32 +0800

    first commit

---
+++ home/static/home/js/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function() {
+    $('.toggle').click(function(){
+      $(this).children('i').toggleClass('fa-pencil');
+      $('.form').animate({
+        height: "toggle",
+        'padding-top': 'toggle',
+        'padding-bottom': 'toggle',
+        opacity: "toggle"
+      }, "slow");
+    });
+});
---
+++ home/views.py
@@ -0,0 +1,55 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.shortcuts import render
+from .forms import  UserForm
+
+def index(request):
+    if not request.user.is_authenticated():
+        return render(request, 'home/login.html')
+    else:
+     return render(request, 'home/index.html')
+
+
+def logout_user(request):
+    logout(request)
+    form = UserForm(request.POST or None)
+    context = {
+        "form": form,
+    }
+    return render(request, 'home/login.html', {"form": form,})
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return render(request, 'home/index.html')
+            else:
+                return render(request, 'home/login.html', {'error_message': 'Your account has been disabled'})
+        else:
+            return render(request, 'home/login.html', {'error_message': 'Invalid login'})
+    return render(request, 'home/login.html')
+
+
+def register(request):
+    form = UserForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user.set_password(password)
+        user.save()
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+
+                return render(request, 'home/index.html')
+    context = {
+        "form": form,
+    }
+    return render(request, 'home/register.html', context)
